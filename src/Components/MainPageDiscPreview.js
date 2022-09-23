68a4130a856f3d0e936fe47d9d4694510292bc29
johnfinley <johnfinleybremerton@gmail.com>
2022-09-23 15:20:20 -0600

    added image alt attrbiute

--- src/Components/MainPageDiscPreview.js
+++ src/Components/MainPageDiscPreview.js
@@ -5,7 +5,7 @@ function MainPageDiscPreview({disc}) {
     <div className="w3-col l3 s6">
       <div className="w3-container">
         <div className="w3-display-container">
-          <img src={disc.picture_url} style={{ width: "100%" }} />
+          <img src={disc.picture_url} style={{ width: "100%" }} alt="discpreview" />
           <div className="w3-display-middle w3-display-hover">
             <button onClick={() => console.log(disc.id)} className="w3-button w3-black">
               More Info <i className="fa fa-shopping-cart"></i>
